8eaf16cf1570089b86c89c7d40b5409a4a4f2877
Francesco Baldisserri <>
2019-05-25 12:25:15 +0200

    Added luenberger observer files, and matrix utils

--- applications/applications.mk
+++ applications/applications.mk
@@ -4,6 +4,10 @@ APPSRC =	applications/app.c \
 			applications/app_sten.c \
 			applications/app_uartcomm.c \
 			applications/app_nunchuk.c \
+			applications/utils/matrix.c \
+			applications/luenberger/luenberger_observer.c \
 			applications/app_custom.c
 
-APPINC = applications
+APPINC = 	applications/utils \
+			applications/luenberger \
+			applications
